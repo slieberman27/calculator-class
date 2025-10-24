1c67b3c98e600b5137f3eeb58c842878dbd1cc33
slieberman27 <slieberman@browning.edu>
2025-10-24 17:17:46 +0000

    did operations

--- main.py
+++ main.py
@@ -2,3 +2,20 @@ num1 = float(input("enter first number: "))
 num2 = float(input("enter second number: "))
 
 operation = input("enter your operation (+ (addition), - (subtraction), * (multiplication), / (division)): ")
+
+if operation == "+":
+    answer = num1 + num2
+    print(answer)
+elif operation == "-":
+    answer = num1 - num2
+    print(answer)
+elif operation == "*":
+    answer = num1 * num2
+    print(answer)
+elif operation == "/":
+    answer = num1 / num2
+    print(answer)
+else:
+    print("error: invalid operation entered")
+
+
